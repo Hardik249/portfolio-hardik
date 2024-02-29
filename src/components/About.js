4ebb2eae7b4597a32feec59d3953619fdbf5307b
Hardik Kotak <hardikkotak249@gmail.com>
2024-02-29 19:24:05 +0530

    Title, About, Skills and Footer sections components data content proper done

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About() {
             <h2 className="text-2xl">About Me</h2>
             {/* <p>Your introduction goes here.</p> */}
             {/* <h1>My Portfolio</h1> */}
-            <p>This is my portfolio website.</p>
+            <p>This is Hardik Kotak portfolio website.</p>
             <p>I am a software developer with experience of 2 Years.</p>
             <p>I am passionate about building web applications that are both user-friendly and efficient.</p>
         </section>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
         <div className="mt-8 pb-4">
-            <p> Made with ❤️  by <a href="https://github.com/HardikDK/portfolio-hardik.git"> Hardik Kotak </a> in React.js </p>
+            <p> Made with ❤️  by <a href="https://github.com/hardik249/portfolio-hardik.git"> Hardik Kotak </a> in React.js </p>
         </div>
     )
 }
